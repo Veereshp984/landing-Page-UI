59d8a975910622580192ff969ad1ad4e77d4114d
veeresh <pasareveeresh0908@gmail.com>
2025-11-29 17:31:58 +0530

    completed NavBar styling

---
+++ src/components/section1/navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div className='flex items-center justify-between px-8 py-5'>
+      <h4 className='bg-black text-white px-5 py-2 rounded-full uppercase'>Target Audience</h4>
+      <button className='bg-gray-200 px-5 py-2 rounded-full uppercase '>Digital banking platfrom</button>
+    </div>
+  )
+}
+
+export default navbar
---
+++ src/components/section1/section1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './navbar'
+const section1 = () => {
+  return (
+    <div className='h-screen w-full '>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default section1
